9be29f49667ca7d79b96218b06b01e886d94787a
christhopher.leao <christhopherleao@icloud.com>
2016-05-24 22:42:57 -0300

    todo: acabamento
    ver se usabilidade dos filtros tá boa

--- js/application.js
+++ js/application.js
@@ -7,9 +7,15 @@ var app = angular.module('materialPage', ['ngMaterial', 'ngRoute']);
   }]);
 
   app.config(function($mdThemingProvider) {
+  var ctaTeal = $mdThemingProvider.extendPalette('teal', {
+  'A200': '26a69a',
+  'A100': '4db6ac',
+  'contrastLightColors': ['300', '400', 'A100', 'A200']
+  });
+  $mdThemingProvider.definePalette('plugTeal', ctaTeal);
   $mdThemingProvider.theme('default')
     .primaryPalette('purple')
-    .accentPalette('light-blue')
+    .accentPalette('plugTeal')
 });
 
   app.controller('PageCtrl', function ($scope, $location, $http) {
